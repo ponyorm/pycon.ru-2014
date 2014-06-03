115d575fa70f81a15e3ac6a2cb8a4728e1d3a2c5
Alexey Malashkevich <m.alexey@gmail.com>
2014-06-03 09:39:38 +0400

    Add Tornado, Jinja2, photos.html stub

---
+++ main.py
@@ -0,0 +1,23 @@
+import tornado.web, tornado.ioloop
+from jinja2 import Environment, FileSystemLoader
+
+template_env = Environment(loader=FileSystemLoader(searchpath="templates"))
+
+class BaseHandler(tornado.web.RequestHandler):
+    def render(self, file_name, **kwargs):
+        template = template_env.get_template(file_name)
+        self.write(template.render(**kwargs))
+
+class MainHandler(BaseHandler):
+    def get(self):
+        self.render("photos.html")
+
+if __name__ == "__main__":
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+        ],
+        debug=True
+    )
+    app.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
