c56122778d07c48441742a59d2399a0f9c84c91c
Alexey Malashkevich <m.alexey@gmail.com>
2014-06-03 10:07:16 +0400

    Process POST requests for login and signup, cookie_secret, login_url, error message for signup, login

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os.path
 import tornado.web, tornado.ioloop
 from jinja2 import Environment, FileSystemLoader
+from entities import *
 
 template_env = Environment(loader=FileSystemLoader(searchpath="templates"))
 
@@ -16,10 +17,32 @@ class MainHandler(BaseHandler):
 class LoginHandler(BaseHandler):
     def get(self):
         self.render('login.html')
+    @db_session
+    def post(self):
+        username = self.get_argument('username', '')
+        password = self.get_argument('password', '')
+        user = User.get(username=username, password=password)
+        if user is None:
+            self.render('login.html', error='Login and password do not match')
+        else:
+            self.set_secure_cookie('username', username)
+            self.redirect('/')
 
 class SignupHandler(BaseHandler):
     def get(self):
         self.render('signup.html')
+    @db_session
+    def post(self):
+        username = self.get_argument('username', '')
+        password = self.get_argument('password', '')
+        if not username or not password:
+            self.render('signup.html', error='Please specify username and password')
+        elif User.exists(username=username):
+            self.render('signup.html', error='Username is already taken')
+        else:
+            User(username=username, password=password)
+            self.set_secure_cookie('username', username)
+            self.redirect('/')
 
 if __name__ == "__main__":
     app = tornado.web.Application(
@@ -28,6 +51,8 @@ if __name__ == "__main__":
             (r"/login", LoginHandler),
             (r"/signup", SignupHandler),
         ],
+        cookie_secret='Secret Cookie',
+        login_url="/login",
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         debug=True
     )
